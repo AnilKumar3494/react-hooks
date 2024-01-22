01e548ffd9a44883670957c56d12dab82a33584d
Anil <karapaanilkumar005@gmail.com>
2024-01-22 18:19:51 -0500

    useState() - with multiple hooks, implemented counter and under stood why to use inner functions

--- react-hooks/src/LearninguseState.jsx
+++ react-hooks/src/LearninguseState.jsx
@@ -1,16 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+//if default state  is passed directly it gets rendered for every click
+function defaultState() {
+  console.log("This is running");
+  return 5;
+}
 const LearninguseState = () => {
-  let count = 0;
+  //   const [count, setCount] = useState(() => {
+  //     return defaultState();
+  //   });
+
+  //   //using objects in useState()
+  //   const [state, setState] = useState({
+  //     color: "blue",
+  //     count: 6,
+  //   });
+
+  //   //we need to pull them about
+  //   let color = state.color;
+  //   let count = state.count;
+
+  //using multiple hooks
+  const [count, setCount] = useState(defaultState);
+  //for theme
+  const [theme, setTheme] = useState("");
+
+  function incrementCounter() {
+    setCount((prevCountValue) => {
+      return prevCountValue + 1;
+    });
+    setTheme("Green");
+  }
+
+  function decrementCounter() {
+    setCount((prevCount) => prevCount - 1);
+    setTheme("red");
+  }
+
+  //Learning
+  //   //This is an incorrect way as this
+  //   function incrementCounter() {
+  //     //We are essentailly caliing the exact same thing twice - they are overriding each other
+  //     setCount(count + 1);
+  //     setCount(count + 1);
+  //   }
+
+  /*    //Doing in the right way
+
+  function incrementCounter() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function decrementCounter() {
+    setCount((prevCount) => {
+      return prevCount - 1;
+    });
+  }
+*/
 
   return (
     <div>
-      <div>
-        <button>Increment</button>
-        <button>Decrement</button>
-        <span>Counter: {count}</span>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+        }}
+      >
+        <button onClick={incrementCounter}>Increment</button>
+        <button onClick={decrementCounter}>Decrement</button>
       </div>
+      <p
+        style={{
+          color: theme,
+        }}
+      >
+        Counter: {count}
+      </p>
     </div>
   );
 };
