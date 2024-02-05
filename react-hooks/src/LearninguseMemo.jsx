4dfba63ad88b605dd9d68096146dab9c3e9a2329
Anil <karapaanilkumar005@gmail.com>
2024-02-04 23:59:01 -0500

    using the useMemo hook

--- react-hooks/src/LearninguseMemo.jsx
+++ react-hooks/src/LearninguseMemo.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const LearninguseMemo = () => {
   const [inputNumber, setInputNumber] = useState(2);
-  let doubler = inputNumber * 2;
+
+  //doubling the inputNumber and to see how it can increase slowness.
+  //   const doubler = slowFunction(inputNumber);
+
+  //now using useMemo() to sort of cache the value and only the funcation when the 'inputNnumber' changes.
+  const doubler = useMemo(() => {
+    return slowFunction(inputNumber);
+  }, [inputNumber]);
+
+  //theme change
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const themeStyle = {
+    padding: "0.7rem",
+    backgroundColor: darkTheme ? "grey" : "white",
+    color: darkTheme ? "white" : "black",
+  };
 
   //Front end
   return (
@@ -13,15 +29,27 @@ const LearninguseMemo = () => {
           type="number"
           value={inputNumber}
           onChange={(e) => {
-            setInputNumber(e.target.value);
+            setInputNumber(parseInt(e.target.value));
           }}
         />
-        <p>{inputNumber}</p>
-        <button>Change Theme</button>
+        {/* <p>{inputNumber}</p> */}
+        <button
+          onClick={() => {
+            setDarkTheme((prevDark) => !prevDark);
+          }}
+        >
+          Change Theme
+        </button>
       </div>
-      <h3>Doubling Input: {doubler}</h3>
+      <h3 style={themeStyle}>Doubling Input: {doubler}</h3>
     </div>
   );
 };
 
 export default LearninguseMemo;
+
+//this function slows down our application as it needs to iterate through all the i's and it gets re-rendered for every click
+function slowFunction(numFromInput) {
+  for (let i = 0; i <= 100000000; i++) {}
+  return numFromInput * 2;
+}
